fd2a1c2081086cc802f07256de6755e3bc483da0
Eder Eduardo <eder.esilva@gmail.com>
2015-07-23 02:32:54 -0300

    now insert image in body send path of image

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     $('#button-send').on('click', function () {
-        //var $input = $('#input-path').val();
-        //app.view.container();
-        chrome.tabs.executeScript(function () {
-            alert("its Works");
+        var $input = $('#input-path').val();
+        if (!$input) {
+            return;
+        }
+        var object = { 'path' : $input };
+        chrome.tabs.executeScript(null, {
+            code: 'var object = ' + JSON.stringify(object)
+        }, function  () {
+            chrome.tabs.executeScript(null, {  file: 'js/julius.js'});
         });
     });
 });
